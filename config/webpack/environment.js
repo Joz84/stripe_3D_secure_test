50c8d142faad71db8a3244134eef4b3905d2cf7c
Claudine <clo.pradal@gmail.com>
2019-08-06 07:52:47 +0200

    Initial commit of Mihivai App

---
+++ config/webpack/environment.js
@@ -0,0 +1,17 @@
+const { environment } = require('@rails/webpacker')
+
+  // Bootstrap 3 has a dependency over jQuery:
+const webpack = require('webpack')
+
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+// Bootstrap 4 has a dependency over jQuery & Popper.js:
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
+
+module.exports = environment
